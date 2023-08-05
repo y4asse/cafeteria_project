6b6a40fb6922965af9149890046979e14ef3ea4b
y4asse <yasse0218@icloud.com>
2023-08-05 22:13:22 +0900

    comment

--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Layout from '../layout';
 import {GetServerSideProps, GetStaticPaths, GetStaticProps} from 'next';
 import {format} from 'date-fns';
-import {Flex} from '@chakra-ui/react';
+import {Button, Flex} from '@chakra-ui/react';
+import {useSession} from 'next-auth/react';
+import {useRouter} from 'next/router';
 
 type Post = {
   id: string;
@@ -65,7 +67,41 @@ type Props = {
   comments: Comment[];
 };
 const Post = (props: Props) => {
+  const router = useRouter();
+  const {data: session} = useSession();
   const {post, comments} = props;
+  const [comment, setComment] = React.useState('');
+  const submit = async () => {
+    if (session === (undefined || null)) {
+      alert('ログインしてください');
+      return;
+    }
+    if (comment === '') {
+      alert('コメントを入力してください');
+      return;
+    }
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/comments/create`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content: comment,
+          pid: post.id,
+          uid: session.user.id,
+        }),
+      }
+    );
+    if (res.ok) {
+      setComment('');
+      router.reload();
+      return;
+    }
+    alert('エラーが起こりました');
+  };
   return (
     <Layout>
       <div>
@@ -94,32 +130,14 @@ const Post = (props: Props) => {
           {comments.map((comment) => (
             <div
               key={comment.id}
-              className="border-2 border-black p-5 flex gap-3  shadow-2xl m-5 rounded-xl">
+              className="border-2 border-black p-5 flex gap-3  shadow-lg my-5 mb-10 mx-24 rounded-xl">
               <img
                 className="w-20 h-20 rounded-full"
-                src={comment.user.profileImageUrl}
-                alt="comment-user-profile-profileImageUrl"
-              />
-              <div className=" rounded">
-                <p className="font-bold">
-                  {comment.user.username}
-                  <span className="font-thin">
-                    {format(Date.parse(comment.created_at), 'yyyy/MM/dd')}
-                  </span>
-                </p>
-                {comment.content}
-              </div>
-              <br />
-            </div>
-          ))}
-
-          {comments.map((comment) => (
-            <div
-              key={comment.id}
-              className="border-2 border-black p-5 flex gap-3  shadow-lg m-5 rounded-xl">
-              <img
-                className="w-20 h-20 rounded-full"
-                src={comment.user.profileImageUrl}
+                src={
+                  comment.user.profileImageUrl
+                    ? comment.user.profileImageUrl
+                    : '/images/icon.png'
+                }
                 alt="comment-user-profile-profileImageUrl"
               />
               <div className=" rounded">
@@ -135,8 +153,20 @@ const Post = (props: Props) => {
             </div>
           ))}
         </div>
-        <br />
-        <br />
+
+        <div className="w-full px-36">
+          <h1 className="text-2xl  my-5 ">コメントする</h1>
+          <textarea
+            className="p-3 border mb-3 border-gray-500 w-full mx-auto  text-xl rounded-xl shadow"
+            value={comment}
+            onChange={(e) => {
+              setComment(e.target.value);
+            }}
+          />
+          <Button className="mb-5" onClick={submit}>
+            コメントする
+          </Button>
+        </div>
       </div>
     </Layout>
   );
