77a740d1c54cca8ae88cbd83e57511c3dc2bea64
y4asse <yasse0218@icloud.com>
2023-08-05 20:32:02 +0900

    validation, getserversideprops

--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../layout';
-import {GetStaticPaths, GetStaticProps} from 'next';
+import {GetServerSideProps, GetStaticPaths, GetStaticProps} from 'next';
 import {format} from 'date-fns';
 
 type Post = {
@@ -31,27 +31,27 @@ type User = {
   image: string;
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
-  const posts: Post[] = await data.json();
-  const paths = posts.map((post) => ({params: {id: post.id}}));
-  return {paths, fallback: false};
-};
-
-export const getStaticProps: GetStaticProps = async ({params}) => {
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
   const {id} = params!;
   const postData = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/posts/${id}`
   );
-  const post = await postData.json();
-  const pid = post.id;
-  const commentData = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/comments/${pid}`
-  );
-  const comments: Comment[] = await commentData.json();
-  return {
-    props: {post, comments},
-  };
+  try {
+    const post = await postData.json();
+    const pid = post.id;
+    const commentData = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/comments/${pid}`
+    );
+    const comments: Comment[] = await commentData.json();
+    return {
+      props: {post, comments},
+    };
+  } catch (e) {
+    //notfound
+    return {
+      notFound: true,
+    };
+  }
 };
 
 type Props = {
