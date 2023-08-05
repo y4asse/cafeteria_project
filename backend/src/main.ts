bd699705cb0135220453d48ab6bf3f599f9469af
y4asse <yasse0218@icloud.com>
2023-08-06 00:51:48 +0900

    delete aws

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { ServerlessNestjsApplicationFactory } from 'serverless-lambda-nestjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,20 +12,3 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
-
-if (process.env.NX_CLI_SET) {
-  bootstrap();
-}
-
-// Run Nestjs application in AWS Lambda
-export const handler: APIGatewayProxyHandler = async (event, context) => {
-  const app = new ServerlessNestjsApplicationFactory<AppModule>(AppModule, {
-    // NestFactory.create's option object
-    cors: {
-      origin: '*',
-      allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-    },
-  });
-  const result = await app.run(event, context);
-  return result;
-};
